82f3f7b354aa6ef39f1cbfb380d5d3e7b8564513
luoweida <3275365883@qq.com>
2023-04-14 22:20:00 +0800

    取值范围，柱状图数字居中

--- Shared/Charts/BarChart/BarChart.cs
+++ Shared/Charts/BarChart/BarChart.cs
@@ -7,38 +7,65 @@ namespace Shared.Charts
     public partial class BarChart : Chart
     {
         public int[]? Data;
+        public int[] maxAndMin;
         public Color LineColor = Colors.WebGray;
-
+        public int[] maxValue(int[] num)
+        {
+            int[] arr = {num[0],num[0]};
+            for (int i = 1; i < num.Length; i++)
+            {
+                arr[0] = arr[0] > num[i] ? arr[0] : num[i];
+                arr[1] = arr[1] < num[i] ? arr[1] : num[i];
+            }
+            return arr;
+        }
         public override void _Ready()
         {
-            // 最高200
+            // Data = new int[] {
+            //     25,
+            //     75,
+            //     125,
+            //     175,
+            //     100,
+            //     20,
+            //     185,
+            //     215,
+            //     55
+            // };
             Data = new int[] {
-                25,
-                75,
-                125,
+                129,
+                275,
+                1225,
                 175,
-                100,
                 20,
+                600,
                 185,
                 215,
-                55
+                900
             };
+            maxAndMin = maxValue(Data);
         }
-
         public override void _Draw()
         {
             base._Draw();
             Font font = ResourceLoader.Load<FontFile>("res://Render/PingFang-SC-Regular.ttf");
-            for(int i=0;i<5;i++)
+
+            int maxl = maxAndMin[0].ToString().Length;
+            int max = (maxAndMin[0] - maxAndMin[0] % (int)Math.Pow(10,maxl-2)) + (int)Math.Pow(10,maxl-2);
+            int step = (int)max/4;
+
+            for (int i = 0; i < 5; i++)
             {
-                DrawLine(new Vector2(50,250-i*50), new Vector2(55+Data!.Length*50,250-i*50), Colors.WebGray);
-                DrawString(font, new Vector2(10, 255-50*i),(i*50).ToString(),fontSize:14);
+                DrawLine(new Vector2(50, 250 - i * 50), new Vector2(55 + Data!.Length * 50, 250 - i * 50), Colors.WebGray);
+                DrawString(font, new Vector2(40 - 8*(i * step).ToString().Length, 255 - 50 * i), (i * step).ToString(), fontSize: 14);
             }
-            DrawLine(new Vector2(50,250), new Vector2(50,25), Colors.WebGray);
+            DrawLine(new Vector2(50, 250), new Vector2(50, 25), Colors.WebGray);
             for (int i = 0; i < Data!.Length; i++)
             {
-                DrawRect(new Rect2(60+50*i, 250-Data[i], 25, Data[i]), Colors.Green);
-                DrawString(font, new Vector2(60+50*i, 240-Data[i]),(Data[i]).ToString(),fontSize:14);
+                int h = 200*Data[i]/max;
+                DrawRect(new Rect2(60 + 50 * i, 250 - h, 25, h), Colors.Green);
+                double x = (60 + 50*i + 12.5) - 8*(Data[i]).ToString().Length/2;
+                DrawString(font, new Vector2((int)x, 240 - h), (Data[i]).ToString(), fontSize: 14);
             }
         }
     }
--- Shared/Charts/LineChart/LineChart.cs
+++ Shared/Charts/LineChart/LineChart.cs
@@ -7,36 +7,69 @@ namespace Shared.Charts
     public partial class LineChart : Chart
     {
         public Vector2[]? Data;
+        public int[] maxAndMin;
         public Color LineColor = Colors.WebGray;
-
+        public int[] maxValue(int[] num)
+        {
+            int[] arr = {num[0],num[0]};
+            for (int i = 1; i < num.Length; i++)
+            {
+                arr[0] = arr[0] > num[i] ? arr[0] : num[i];
+                arr[1] = arr[1] < num[i] ? arr[1] : num[i];
+            }
+            return arr;
+        }
         public override void _Ready()
         {
+            // Data = new Vector2[] {
+            //     new Vector2(75, 150),
+            //     new Vector2(125, 90),
+            //     new Vector2(175, 250),
+            //     new Vector2(225, 210),
+            //     new Vector2(275, 270),
+            //     new Vector2(325, 160),
+            //     new Vector2(375, 130),
+            //     new Vector2(425, 130),
+            //     new Vector2(475, 60),
+            // };
             Data = new Vector2[] {
-                new Vector2(75, 150),
-                new Vector2(125, 90),
-                new Vector2(175, 250),
-                new Vector2(225, 210),
-                new Vector2(275, 270),
+                new Vector2(75, 200),
+                new Vector2(125, 290),
+                new Vector2(175, 1250),
+                new Vector2(225, 1210),
+                new Vector2(275, 2270),
                 new Vector2(325, 160),
                 new Vector2(375, 130),
-                new Vector2(425, 130),
-                new Vector2(475, 60),
+                new Vector2(425, 350),
+                new Vector2(475, 900),
             };
+            int[] arr = new int[Data!.Length];
+            for(int i=0;i<Data!.Length;i++){arr[i] = (int)Data[i].Y;}
+            maxAndMin = maxValue(arr);
         }
 
         public override void _Draw()
         {
             base._Draw();
             Font font = ResourceLoader.Load<FontFile>("res://Render/PingFang-SC-Regular.ttf");
+
+            int maxl = maxAndMin[0].ToString().Length;
+            int max = (maxAndMin[0] - maxAndMin[0] % (int)Math.Pow(10,maxl-2)) + (int)Math.Pow(10,maxl-2);
+            int step = (int)max/4;
+
             for(int i=0;i<5;i++)
             {
                 DrawLine(new Vector2(50,250-i*50), new Vector2(50+Data!.Length*50,250-i*50), Colors.WebGray);
-                DrawString(font, new Vector2(10, 255-50*i),(i*50).ToString(),fontSize:14);
+                DrawString(font, new Vector2(40 - 8*(i * step).ToString().Length, 255-50*i),(i*step).ToString(),fontSize:14);
             }
             for (int i = 1; i < Data!.Length; i++)
             {
-                Vector2 from = new Vector2(Data[i - 1].X, Height - Data[i - 1].Y);
-                Vector2 to = new Vector2(Data[i].X, Height - Data[i].Y);
+                // Vector2 from = new Vector2(Data[i - 1].X, Height - Data[i - 1].Y);
+                // Vector2 to = new Vector2(Data[i].X, Height - Data[i].Y);
+                int h1 = (int)(200*Data[i-1].Y/max);
+                int h2 = (int)(200*Data[i].Y/max);
+                Vector2 from = new Vector2(Data[i - 1].X, 250 - h1);
+                Vector2 to = new Vector2(Data[i].X, 250 - h2);
                 DrawLine(from, to, LineColor);
             }
         }
