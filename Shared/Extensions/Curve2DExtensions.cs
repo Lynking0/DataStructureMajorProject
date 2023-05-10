ef15f2fc1afc727635e7c5aca5fd85e33a87f006
Lynking <2910793738@qq.com>
2023-05-10 21:55:56 +0800

    一定改好了！

--- Shared/Extensions/Curve2DExtensions.cs
+++ Shared/Extensions/Curve2DExtensions.cs
@@ -34,31 +34,45 @@ namespace Shared.Extensions.Curve2DExtensions
         /// <summary>
         ///   将当前曲线与若干曲线合并，不改变原曲线
         /// </summary>
-        /// <returns>合并后的曲线，合并失败则返回null</returns>
-        public static Curve2D? Concat(this Curve2D thisCurve, params Curve2D[] curves)
+        /// <returns>合并后的曲线，合并失败则报错</returns>
+        public static Curve2D Concat(this Curve2D thisCurve, params Curve2D[] curves)
         {
             IEnumerable<(Vector2 pos, Vector2 @in, Vector2 @out)> points;
-            if (thisCurve.GetPointPosition(0).IsEqualApprox(curves[0].GetPointPosition(0)))
-                points = thisCurve.GetEnumerableReverse().Concat(curves[0].GetEnumerable());
-            else if (thisCurve.GetPointPosition(0).IsEqualApprox(curves[0].GetPointPosition(curves[0].PointCount - 1)))
-                points = thisCurve.GetEnumerableReverse().Concat(curves[0].GetEnumerableReverse());
-            else if (thisCurve.GetPointPosition(thisCurve.PointCount - 1).IsEqualApprox(curves[0].GetPointPosition(0)))
-                points = thisCurve.GetEnumerable().Concat(curves[0].GetEnumerable());
-            else if (thisCurve.GetPointPosition(thisCurve.PointCount - 1).IsEqualApprox(curves[0].GetPointPosition(curves[0].PointCount - 1)))
-                points = thisCurve.GetEnumerable().Concat(curves[0].GetEnumerableReverse());
-            else
-                return null;
             Curve2D newCurve = new Curve2D();
-            foreach ((Vector2 pos, Vector2 @in, Vector2 @out) in points)
-                newCurve.AddPoint(pos, @in, @out);
+            Curve2D lastCurve = thisCurve;
             for (int i = 1; i < curves.Length; ++i)
             {
-                if (newCurve.GetPointPosition(newCurve.PointCount - 1).IsEqualApprox(curves[i].GetPointPosition(0)))
-                    points = curves[i].GetEnumerable();
-                else if (newCurve.GetPointPosition(newCurve.PointCount - 1).IsEqualApprox(curves[i].GetPointPosition(curves[i].PointCount - 1)))
-                    points = curves[i].GetEnumerableReverse();
+                if (newCurve.PointCount == 0)
+                {
+                    if (lastCurve.PointCount == 0)
+                    {
+                        lastCurve = curves[i];
+                        continue;
+                    }
+                    else if (curves[i].PointCount == 0)
+                        continue;
+                    else if (lastCurve.GetPointPosition(0).IsEqualApprox(curves[i].GetPointPosition(0)))
+                        points = lastCurve.GetEnumerableReverse().Concat(curves[i].GetEnumerable());
+                    else if (lastCurve.GetPointPosition(0).IsEqualApprox(curves[i].GetPointPosition(curves[i].PointCount - 1)))
+                        points = lastCurve.GetEnumerableReverse().Concat(curves[i].GetEnumerableReverse());
+                    else if (lastCurve.GetPointPosition(lastCurve.PointCount - 1).IsEqualApprox(curves[i].GetPointPosition(0)))
+                        points = lastCurve.GetEnumerable().Concat(curves[i].GetEnumerable());
+                    else if (lastCurve.GetPointPosition(lastCurve.PointCount - 1).IsEqualApprox(curves[i].GetPointPosition(curves[i].PointCount - 1)))
+                        points = lastCurve.GetEnumerable().Concat(curves[i].GetEnumerableReverse());
+                    else
+                        throw new Exception("Shared.Extensions.Curve2DExtensions.Concat(): Value error!");
+                }
                 else
-                    return null;
+                {
+                    if (curves[i].PointCount == 0)
+                        continue;
+                    else if (newCurve.GetPointPosition(newCurve.PointCount - 1).IsEqualApprox(curves[i].GetPointPosition(0)))
+                        points = curves[i].GetEnumerable();
+                    else if (newCurve.GetPointPosition(newCurve.PointCount - 1).IsEqualApprox(curves[i].GetPointPosition(curves[i].PointCount - 1)))
+                        points = curves[i].GetEnumerableReverse();
+                    else
+                        throw new Exception("Shared.Extensions.Curve2DExtensions.Concat(): Value error!");
+                }
                 foreach ((Vector2 pos, Vector2 @in, Vector2 @out) in points)
                     newCurve.AddPoint(pos, @in, @out);
             }
