8798231ef5d82bc3950b557bbaf298b53fd21005
0x587 <3225678419@qq.com>
2023-05-17 18:02:55 +0800

    for debug

--- Transport/Trip.cs
+++ Transport/Trip.cs
@@ -1,11 +1,21 @@
+using System.Diagnostics;
+using IndustryMoudle.Extensions;
 using GraphMoudle;
 
 namespace TransportMoudle
 {
+    [DebuggerDisplayAttribute("CommandLine: {DebuggerDisplay}")]
     public struct Trip
     {
         public TrainLine Line;
         public Vertex Start;
         public Vertex End;
+        public string DebuggerDisplay
+        {
+            get
+            {
+                return $"{Start.GetFactory()!.ID} -> {End.GetFactory()!.ID} by {Line.ID}";
+            }
+        }
     }
 }
